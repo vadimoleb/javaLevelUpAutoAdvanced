52c7cd54f7cc7876e71cefdec92547e08fa8d3e0
vadim <vadimoleb@mail.ru>
2023-12-11 21:35:11 +0300

    start course

---
+++ src/test/java/ru/levelpp/at/advance/lesson0203/taf/trello/test/BoardsTest.java
@@ -0,0 +1,11 @@
+package ru.levelpp.at.advance.lesson0203.taf.trello.test;
+
+import org.junit.jupiter.api.Test;
+
+public class BoardsTest {
+
+    @Test
+    void createTest(){
+        //сгенерировать название
+    }
+}

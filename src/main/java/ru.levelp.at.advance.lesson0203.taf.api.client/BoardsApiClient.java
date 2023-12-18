24c83504b11df553f530f07dc485f404cc1b7fc6
vadim <vadimoleb@mail.ru>
2023-12-18 11:36:04 +0300

    start course

---
+++ src/main/java/ru.levelp.at.advance.lesson0203.taf.api.client/BoardsApiClient.java
@@ -0,0 +1,10 @@
+package ru.levelp.at.advance.lesson0203.taf.api.client;
+
+import javax.xml.ws.Response;
+import java.util.Map;
+
+
+public class BoardsApiClient {
+    public static final  String BOARDS_ENDPOINT = "/boards";
+
+}
